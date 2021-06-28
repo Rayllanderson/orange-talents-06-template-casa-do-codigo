39b9d0443d440b8ada5f2ee3d0306117c2a13028
Rayllanderson <rayllanderson@gmail.com>
2021-06-28 19:26:44 -0300

    Removendo not found exception

--- src/main/java/br/com/zupacademy/rayllanderson/casadocodigo/book/controllers/FinderBookController.java
+++ src/main/java/br/com/zupacademy/rayllanderson/casadocodigo/book/controllers/FinderBookController.java
@@ -4,13 +4,14 @@ import br.com.zupacademy.rayllanderson.casadocodigo.book.model.Book;
 import br.com.zupacademy.rayllanderson.casadocodigo.book.repository.BookRepository;
 import br.com.zupacademy.rayllanderson.casadocodigo.book.responses.BookDetailsResponse;
 import br.com.zupacademy.rayllanderson.casadocodigo.book.responses.BookListResponse;
-import br.com.zupacademy.rayllanderson.casadocodigo.core.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class FinderBookController {
 
     @GetMapping("/{id}")
     public ResponseEntity<BookDetailsResponse> findById(@PathVariable Long id) {
-        Book bookToBeSearched = bookRepository.findById(id).orElseThrow(() -> new NotFoundException("Livro não encontrado"));
+        Book bookToBeSearched = bookRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return ResponseEntity.ok(new BookDetailsResponse(bookToBeSearched));
     }
 }
--- src/main/java/br/com/zupacademy/rayllanderson/casadocodigo/core/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.zupacademy.rayllanderson.casadocodigo.core.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
